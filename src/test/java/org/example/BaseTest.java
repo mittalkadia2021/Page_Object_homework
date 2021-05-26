a912f023897127649e95fca29d074a255fe931a6
Mittal <mittalkadia2021@gamil.com>
2021-05-26 20:06:55 +0100

    selenium Homework

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,8 +1,10 @@
 package org.example;
 
+import org.openqa.selenium.By;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 
 public class BaseTest extends Utils {
@@ -15,6 +17,9 @@ public class BaseTest extends Utils {
         //open URL
         driver.get("https://demo.nopcommerce.com/");
     }
+
+
+
 
     @AfterMethod
     public void close_Browser(ITestResult result) {
